c061d1837641134d97a11103080c33a8b4900718
mbp13 <shaxobiddina20@gmail.com>
2024-02-16 00:46:18 +0500

    text uz de a

--- videoconference_app/urls.py
+++ videoconference_app/urls.py
@@ -1,13 +1,21 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('register/',views.register, name='register'),
-    path('login/',views.login_view, name='login'),
-    path('dashboard/',views.dashboard, name='dashboard'),
-    path('meeting/',views.videocall, name='meeting'),
-    path('logout/',views.logout_view, name='logout'),
-    path('join/',views.join_room, name='join_room'),
-    path('',views.index, name='index'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('meeting/', views.videocall, name='meeting'),
+    path('logout/', views.logout_view, name='logout'),
+    path('join/', views.join_room, name='join_room'),
+    path('', views.index, name='index'),
 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'videoconference_app.views.handling_404'
+handler500 = 'videoconference_app.views.handling_500'
