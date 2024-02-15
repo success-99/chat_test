7bf5cae9693a3c61e75fd79a4424cebac17938f1
mbp13 <shaxobiddina20@gmail.com>
2024-02-15 23:30:44 +0500

    email>username

--- videoconference_app/forms.py
+++ videoconference_app/forms.py
@@ -1,25 +1,74 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
-class RegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-    first_name = forms.CharField(max_length=100, required=True)
-    last_name = forms.CharField(max_length=100, required=True)
+class RegisterForm(forms.ModelForm):
+    # email = forms.CharField(max_length=254, required=True, widget=forms.EmailInput())
+    con_password = forms.CharField(widget=forms.PasswordInput, label='Confirm Password')
 
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'email', 'password1','password2')
-    
-    def save(self, commit=True):
-        user = super(RegisterForm, self).save(commit=False)
-        user.email = self.cleaned_data['email']
-        user.username = self.cleaned_data['email']
-        user.first_name = self.cleaned_data['first_name']
-        user.last_name = self.cleaned_data['last_name']
+        fields = ['first_name', 'last_name', 'password', 'username']
+        widgets = {
+            'password': forms.PasswordInput()
+        }
 
-        if commit:
-            user.save()
-        
-        return user
-    
+    def clean_username(self):
+        username = self.cleaned_data.get("username")
+        if User.objects.filter(username__iexact=username).exists():
+            raise forms.ValidationError("Bunday foydalanuvchi mavjud! Foydalanuvchi nomini o'zgartiring!")
+        if len(username) < 4:
+            raise forms.ValidationError(" Foydalanuvchi nomi 4 ta belgidan kam bo'lmasligi kerak!")
+        if username.isdigit():  # Faqat sonlardan tashkil topganligini tekshirish
+            raise forms.ValidationError("Foydalanuvchi nomi raqamlardan tashkil topishi munkin emas!")
+        return username
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+        if first_name.isdigit():  # Faqat sonlardan tashkil topganligini tekshirish
+            raise forms.ValidationError("Ismingiz raqamlardan tashkil topishi munkin emas!")
+        if len(first_name) < 4:
+            raise forms.ValidationError("Iltimos! Ismingiz 4 ta belgidan kam kiritmang!")
+        return first_name
+
+    def clean_last_name(self):
+        last_name = self.cleaned_data.get('last_name')
+        if last_name.isdigit():  # Faqat sonlardan tashkil topganligini tekshirish
+            raise forms.ValidationError("Familyangiz raqamlardan tashkil topishi munkin emas!")
+        if len(last_name) < 4:
+            raise forms.ValidationError("Iltimos! Familiyangizni 4 ta belgidan kam kiritmang!")
+        return last_name
+
+    def clean_password(self):
+        password = self.cleaned_data['password']
+        if len(password) < 4 or len(password) > 8:
+            raise forms.ValidationError(" Parolingiz 4 ta va 8 ta belgi oralig'ida bo'lishi kerak!")
+        return password
+
+    def clean_con_password(self):
+        password = self.cleaned_data.get('password')  # get() ni ishlatamiz
+        con_password = self.cleaned_data.get('con_password')
+        if password and con_password and password != con_password:
+            raise forms.ValidationError("Parollaringiz bir xil emas!")
+        return con_password
+
+    # def clean_email(self):
+    #     email = self.cleaned_data.get("email")
+    #     if User.objects.filter(email__iexact=email).exists():
+    #         self.add_error("email", _("Bunday email egasi mavjud! Emailni o'zgartiring!"))
+    #     return email
+
+    # def save(self, commit=True):
+    #     user = super(RegisterForm, self).save(commit=False)
+    #     user.email = self.cleaned_data['email']
+    #     user.username = self.cleaned_data['email']
+    #     user.first_name = self.cleaned_data['first_name']
+    #     user.last_name = self.cleaned_data['last_name']
+    #     user.password = self.cleaned_data['password']
+    #
+    #     if commit:
+    #         user.save()
+    #
+    #     return user
+    #
--- videoconference_app/views.py
+++ videoconference_app/views.py
@@ -2,30 +2,47 @@ from django.shortcuts import render, redirect
 from .forms import RegisterForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
+
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
 
+
 def register(request):
+    form = RegisterForm()
+    mydict = {'form': form}
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            user.set_password(user.password)
+            user.save()
             return render(request, 'login.html', {'success': "Registration successful. Please login."})
         else:
+            mydict['form'] = form
             error_message = form.errors.as_text()
             return render(request, 'register.html', {'error': error_message})
-
-    return render(request, 'register.html')
+    return render(request, 'register.html', context=mydict)
+    # if request.method == 'POST':
+    #     form = RegisterForm(request.POST)
+    #     if form.is_valid():
+    #         form.save()
+    #         return render(request, 'login.html', {'success': "Registration successful. Please login."})
+    #     else:
+    #         error_message = form.errors.as_text()
+    #         return render(request, 'register.html', {'error': error_message})
+    #
+    # return render(request, 'register.html')
 
 
 def login_view(request):
-    if request.method=="POST":
-        email = request.POST.get('email')
+    if request.method == "POST":
+        username = request.POST.get('username')
         password = request.POST.get('password')
-        user = authenticate(request, username=email, password=password)
+        user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             return redirect("/dashboard")
@@ -34,18 +51,22 @@ def login_view(request):
 
     return render(request, 'login.html')
 
+
 @login_required
 def dashboard(request):
     return render(request, 'dashboard.html', {'name': request.user.first_name})
+
 
 @login_required
 def videocall(request):
     return render(request, 'videocall.html', {'name': request.user.first_name + " " + request.user.last_name})
 
+
 @login_required
 def logout_view(request):
     logout(request)
     return redirect("/login")
+
 
 @login_required
 def join_room(request):
